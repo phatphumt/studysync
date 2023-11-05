542c22dc43cc60182225a4092ca66f5cdc7e52f4
Phatphum <phumfirebase@gmail.com>
2023-11-05 23:13:10 +0700

    some firestore queries

--- frontend/app/flashcard/list/page.tsx
+++ frontend/app/flashcard/list/page.tsx
@@ -1,26 +1,53 @@
-'use client'
-import { db } from '@/app/config/firebase';
-import { collection, getDocs } from 'firebase/firestore';
-import React, { useEffect } from 'react';
+"use client";
+
+import { useAuth } from "@/app/SessionProvider";
+import { db } from "@/app/config/firebase";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 
 const ListFlashcard = () => {
-	useEffect(() => {
-		async function getFlashcard() {
-			try {
-				const data = await getDocs(collection(db, '/flaschards'))
-				const acutualData = data.docs.map(doc => ({...doc.data(), id: doc.id}))
-				console.log(acutualData)
-			} catch (e) {
-				console.error('error while fecting docs' + e)
-			}
-		}
-		getFlashcard()
-	}, [])
-	return (
-		<div className='p-10'>
-			
-		</div>
-	);
+  const auth = useAuth();
+  const [dt, setDt] = useState<undefined | any[]>();
+
+  useEffect(() => {
+    async function a() {
+      try {
+				if (!auth?.user?.uid) {
+					return;
+				}
+				const uid: string = auth?.user?.uid;
+        const q = query(collection(db, "/flaschards"), where("owner", "==", uid));
+        const data = await getDocs(q);
+        const acutualData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        console.log(acutualData);
+        setDt(acutualData);
+				console.log('no error')
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    a();
+  }, [auth?.user?.uid]);
+	
+  return (
+    <div className="p-10">
+      {dt &&
+        dt.map((i: any) => (
+          <div key={i.id}>
+            <span className="font-bold text-xl">Flashcard ({i.name})</span>
+            <br />
+            {i.flashcards.map((i: any, ia: number) => (
+              <div key={i.question}>
+                {ia + 1} - {i.question} | {i.answer}
+              </div>
+            ))}
+          </div>
+        ))}
+    </div>
+  );
 };
 
 export default ListFlashcard;
