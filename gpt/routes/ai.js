a03bd9afe8ec1b52026bbe952956258f752350c6
Phatphum <ryw41541@rayongwit.ac.th>
2024-02-09 11:58:21 +0700

    remove legacy backend

--- gpt/routes/ai.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const aiFlashcardRouter = require('./ai/aiFlashcards');
-const aiQuizRouter = require('./ai/aiQuiz');
-const aiRouter = Router();
-
-aiRouter.use('/flashcard', aiFlashcardRouter);
-aiRouter.use('/quiz', aiQuizRouter);
-
-module.exports = { aiRouter };
